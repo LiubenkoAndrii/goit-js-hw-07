1a482c3afbb1f666a07e36069ab9046ab3250150
Andrii Liubenko <lubenkoandrew@gmail.com>
2023-04-30 22:46:48 +0300

    add-task-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,46 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "/js/gallery-items.js";
 
-console.log(galleryItems);
+const galleryRef = document.querySelector(".gallery");
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryRef.insertAdjacentHTML("beforeend", galleryMarkup);
+
+function createGalleryMarkup(items) {
+  return items
+    .map(({ preview, original, description }) => {
+      return `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}"/>
+          </a>
+        </li>
+      `;
+    })
+    .join("");
+}
+
+galleryRef.addEventListener("click", onGalleryClick);
+
+function onGalleryClick(event) {
+  event.preventDefault();
+  const { target } = event;
+
+  if (target.nodeName !== "IMG") {
+    return;
+  }
+
+  const largeImageURL = target.dataset.source;
+  const instance = basicLightbox.create(`
+    <img src="${largeImageURL}" width="800" height="600">
+`);
+
+  instance.show();
+
+  document.addEventListener("keydown", onEscapePress);
+
+  function onEscapePress(event) {
+    if (event.code === "Escape") {
+      instance.close();
+      document.removeEventListener("keydown", onEscapePress);
+    }
+  }
+}
